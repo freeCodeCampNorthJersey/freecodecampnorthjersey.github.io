085d6006729b42dcdaeb994f41222d6ef0a8ab7d
bushbass <alex.nielsen@gmail.com>
2019-06-07 09:11:34 -0400

    changed some hero text

--- src/components/hero/index.js
+++ src/components/hero/index.js
@@ -9,7 +9,7 @@ function Hero() {
           We are a group of people learning to code. We were originally brought
           together through freeCodeCamp so we are mainly focused on web
           development and JavaScript, but we welcome coders that use other
-          languages.
+          languages. Please join us!
         </p>
         <a href="#meetups">Come to a meetup</a>
       </article>
