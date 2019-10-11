c323659684139d351cc7543b72960a3853bc6b98
bushbass <alex.nielsen@gmail.com>
2019-10-11 12:07:26 -0400

    created skeleton maker-depot page and worked on layout.js for the same page.  home page should be switched to use layout too eventually

---
+++ src/pages/maker-depot.js
@@ -0,0 +1,20 @@
+import React from "react"
+import Layout from "../components/layout"
+
+function MakerDepot() {
+  return (
+    <div>
+      <Layout>
+        <h1>Maker Depot</h1>
+        <p>
+          this is a terrible version of a page that will tell how great maker
+          depot is for hosting our meetings
+        </p>
+        <p>it should have a link to their home page</p>
+        <p>maybe a google map builit right in</p>
+      </Layout>
+    </div>
+  )
+}
+
+export default MakerDepot
